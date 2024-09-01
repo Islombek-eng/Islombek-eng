cfcacc2e40adacd8cab0ac44746357ba2a4e1ced
Islombek-eng <islombekanoritdinov30@gmail.com>
2024-09-01 19:56:08 +0500

    first commit

---
+++ e_shop/index/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import  Product, Category
+
+# Главная страница
+def home_page(request):
+    #Достаем данные ищ БД
+   all_products = Product.objects.all()
+   all_categories = Category.objects.all()
+    #Передаем данные на фронт
+   context = {'products': all_products, 'categories': all_categories}
+   #Прогружаем html на фронт
+   return render(request, 'home.html', context)
+
+#Страница с товарами по категории
+def category_page(request, pk):
+    #Достаем выбранную категорию
+    category = Category.objects.get(id=pk)
+    #Фильтруем продукты по выбранной категории
+    exact_products = Product.objects.filter(pr_category=category)
+    #Передаем данные на фрннт
+    context = {'products': exact_products}
+    # Прогружаем html файл
+    return render(request, 'category.html', context)
+
+# Страница с выбранным товаром
+def product_page(request, pk):
+  # Достаем выбранный товар
+  product = Product.objects.get(id=pk)
+  # Передаем данные на фронт
+  context = {'product': product}
+  # Прогружаем html файл
+  return render(request, 'product.html', context)
